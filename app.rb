07b7fc5fa02b7bb59a768d810ff143329446e15e
Jessica Lin <jessicaa.lin@gmail.com>
2019-05-14 18:38:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,15 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reverse' do
+    # renders the reverse.erb page
     erb :reverse
   end
 
   post '/reverse' do
+    # receives params from reverse form and renders to reversed.erb
+    original_str = params["string"]
+    @reversed_str = original_str.reverse
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
   end
 end
